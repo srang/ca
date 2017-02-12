549a981623104c9c7b73fe9eb17358f3421c11ed
MichaelB94 <michael.baldwin101@gmail.com>
2017-02-12 15:26:02 -0500

    set-up feature
    created branch, commited initial file before developing

---
+++ src/main/java/com/github/srang/ca/sim/TimerProcessStrategy.java
@@ -0,0 +1,27 @@
+package com.github.srang.ca.sim;
+
+import com.github.srang.ca.model.Canvas;
+import com.github.srang.ca.model.Cell;
+
+/**
+ * Created by MichaelB94 on 2/9/17.
+ */
+public class TimerProcessStrategy implements ProcessStrategy {
+    @Override
+    public Canvas process(Canvas canvas) {
+        Canvas newCanvas = new Canvas(canvas.getHeight(), canvas.getWidth());
+        for (int h = 0; h < canvas.getHeight(); h++) {
+            for (int w = 0; w < canvas.getWidth(); w++) {
+                Cell cell = canvas.getCells().get(h).get(w);
+                if (w == 0) {
+                    Integer value = cell.getValue();
+                    cell.setValue((value+1)%3);
+                } else {
+                    cell.setValue(canvas.getCells().get(h).get(w - 1).getValue() + (w * 2));
+                }
+                newCanvas.getCells().get(h).set(w, cell);
+            }
+        }
+        return canvas;
+    }
+}
