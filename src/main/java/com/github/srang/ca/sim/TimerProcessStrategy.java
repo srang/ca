ed109c57d8d9c3e4863163915c5504b2d88409cb
MichaelB94 <michael.baldwin101@gmail.com>
2017-02-12 17:16:07 -0500

    Altered the timerProcessStrategy

--- src/main/java/com/github/srang/ca/sim/TimerProcessStrategy.java
+++ src/main/java/com/github/srang/ca/sim/TimerProcessStrategy.java
@@ -9,16 +9,12 @@ import com.github.srang.ca.model.Cell;
 public class TimerProcessStrategy implements ProcessStrategy {
     @Override
     public Canvas process(Canvas canvas) {
-        Canvas newCanvas = new Canvas(canvas.getHeight(), canvas.getWidth());
+        Canvas newCanvas = new Canvas(canvas.getHeight(), canvas.getWidth());  // create a new canvas
         for (int h = 0; h < canvas.getHeight(); h++) {
             for (int w = 0; w < canvas.getWidth(); w++) {
-                Cell cell = canvas.getCells().get(h).get(w);
-                if (w == 0) {
-                    Integer value = cell.getValue();
-                    cell.setValue((value+1)%3);
-                } else {
-                    cell.setValue(canvas.getCells().get(h).get(w - 1).getValue() + (w * 2));
-                }
+                Cell cell = canvas.getCells().get(h).get(w); // populate this canvas with cells from an ArrayList
+                
+                cell.setValue(cell.getValue()+1); //increment by one 
                 newCanvas.getCells().get(h).set(w, cell);
             }
         }
